fc65b361fc37e8d192adfc8e32847027d0a804d8
Chris Green <cmgreen210@gmail.com>
2015-02-28 14:52:31 -0800

    Upload and save video to temporary location

--- emotion/views.py
+++ emotion/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+import os
 from forms import VideoForm, ImageForm
 
 
@@ -9,6 +13,11 @@ def get_video(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
+            video_file = request.FILES['video_file']
+            path = default_storage.save('tmp/video/vid',
+                                        ContentFile(video_file.read()))
+            tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+
             return HttpResponseRedirect('/')
     else:
         form = VideoForm()
