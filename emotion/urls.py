90f0983003f0a34510608569768bc5a98db1ea65
Chris Green <cmgreen210@gmail.com>
2015-02-28 16:41:43 -0800

    Add support for uploading images. Both videos and images in same view

--- emotion/urls.py
+++ emotion/urls.py
@@ -3,5 +3,7 @@ import emotion.views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', emotion.views.get_video, name='video'),
+                       url(r'^$', emotion.views.home_page, name='home'),
+                       url(r'^video/$', emotion.views.get_video, name='video'),
+                       url(r'^image/$', emotion.views.get_image, name='image'),
                        )
