7a8eb7b87ab9e95e275952a682c67744b65528eb
Chris Green <cmgreen210@gmail.com>
2015-03-01 17:07:02 -0800

    update dummy and base classifier

--- fec/classifier/classifier_base.py
+++ fec/classifier/classifier_base.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from abc import ABCMeta, abstractmethod
 import numpy as np
 
@@ -13,17 +14,27 @@ class ClassifierBase(object):
     def predict(self, x):
         pass
 
+    @abstractmethod
+    def predict_proba(self, x):
+        pass
+
 
 class DummyClassifier(ClassifierBase):
     def __init__(self, n):
-        self._n = None
+        self._n = n
 
     def fit(self, x, y):
-        self._n = x.shape[1]
+        pass
 
     def predict(self, x):
         if self._n is None:
             raise StandardError('You must call fit before predict!')
         return np.random.randint(0, self._n, size=x.shape[0])
+
+    def predict_proba(self, x):
+        if self._n is None:
+            raise StandardError('You must call fit before predict!')
+        m = x.shape[0]
+        return (1 / self._n) * np.ones((m, self._n))
 
 ClassifierBase.register(DummyClassifier)
