46fe3ddc60d9ac6c854db55abfcaf90378071ddb
Chris Green <cmgreen210@gmail.com>
2015-03-02 11:08:29 -0800

    g2 setup script

---
+++ g2_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo apt-get update
+# CUDA
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_6.5-14_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1204_6.5-14_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# PYTHON STUFF
+sudo apt-get install python-setuptools
+sudo easy_install pip
+
+# GRAPHLAB
+sudo pip install http://static.dato.com/files/graphlab-create-1.3.gpu.tar.gz
