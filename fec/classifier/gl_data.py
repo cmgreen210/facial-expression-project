bc79861cbf0691f851d3145e1a94af6ae0cca89c
Chris Green <cmgreen210@gmail.com>
2015-03-03 10:02:00 -0800

    graphlab stuff

---
+++ fec/classifier/gl_data.py
@@ -0,0 +1,33 @@
+import numpy as np
+import graphlab as gl
+from boto.s3.connection import S3Connection
+import os
+from filechunkio import FileChunkIO
+import math
+
+
+_conn = None
+
+
+def get_connection():
+    global _conn
+    if _conn is None:
+        _conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'],
+                             os.environ['AWS_SECRET_ACCESS_KEY'])
+    return _conn
+
+
+def upload_big_file(source_path, bucket):
+    source_size = os.stat(source_path).st_size
+    mp = bucket.initiate_multipart_upload(os.path.basename(source_path))
+
+    chunk_size = 52428800
+    chunk_count = int(math.ceil(source_size / chunk_size))
+    for i in range(chunk_count + 1):
+        offset = chunk_size * i
+        bytes = min(chunk_size, source_size - offset)
+        with FileChunkIO(source_path, 'r', offset=offset,
+                         bytes=bytes) as fp:
+            mp.upload_part_from_file(fp, part_num=i + 1)
+
+    mp.complete_upload()
