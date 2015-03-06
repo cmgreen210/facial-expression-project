915e04f1a8adddcb65e6252ac01da472869bef5c
Chris Green <cmgreen210@gmail.com>
2015-03-06 13:43:54 -0800

    gl nn builder

--- fec/classifier/gl_nn.py
+++ fec/classifier/gl_nn.py
@@ -1,9 +1,14 @@
 import graphlab as gl
 
 
-class GraphLabNeuralNet(object):
+class GraphLabNeuralNetBuilder(object):
     def __init__(self):
         self.layers = list()
+        self.net = gl.deeplearning.NeuralNet()
+
+    def get_net(self):
+        self.net.layers = self.layers
+        self.net.verify()
 
     def add_convolution_layer(self, kernel_size,
                               stride, num_channels, **kwargs):
@@ -12,3 +17,54 @@ class GraphLabNeuralNet(object):
             stride=stride, **kwargs
         )
         self.layers.append(conv_layer)
+        return
+
+    def add_max_pooling_layer(self, kernel_size, stride=1, padding=0):
+        pool_layer = gl.deeplearning.layers.MaxPoolingLayer(
+            kernel_size=kernel_size, stride=stride, padding=padding
+        )
+        self.layers.append(pool_layer)
+        return
+
+    def add_avg_pooling_layer(self, kernel_size, stride=1, padding=0):
+        pool_layer = gl.deeplearning.layers.AveragePoolingLayer(
+            kernel_size=kernel_size, stride=stride, padding=padding
+        )
+        self.layers.append(pool_layer)
+        return
+
+    def add_flatten_layer(self):
+        self.layers.append(
+            gl.deeplearning.layers.FlattenLayer()
+        )
+        return
+
+    def add_full_connection_layer(self, num_hidden_units,
+                                  init_bias=0, init_sigma=0.01,
+                                  init_random='gaussian'):
+        if len(self.layers) == 0 or self.layers[-1].name != 'flatten':
+            self.add_flatten_layer()
+
+        fc_layer = gl.deeplearning.layers.FullConnectionLayer(
+            num_hidden_units, init_bias=init_bias, init_sigma=init_sigma,
+            init_random=init_random
+        )
+        self.layers.append(fc_layer)
+        return
+
+    def add_relu_layer(self):
+        self.layers.append(gl.deeplearning.layers.RectifiedLinearLayer())
+        return
+
+    def add_sigmoid_layer(self):
+        self.layers.append(gl.deeplearning.layers.SigmoidLayer())
+
+    def add_tanh_layer(self):
+        self.layers.append(gl.deeplearning.layers.TanhLayer())
+
+    def add_soft_plus_layer(self):
+        self.layers.append(gl.deeplearning.layers.SoftplusLayer())
+
+    def add_dropout_layer(self, threshold=0.5):
+        drop_layer = gl.deeplearning.layers.DropoutLayer(threshold)
+        self.layers.append(drop_layer)
--- fec/test/test_gl_nn.py
+++ fec/test/test_gl_nn.py
@@ -1,13 +1,51 @@
 import unittest
-from fec.classifier.gl_nn import GraphLabNeuralNet
+from fec.classifier.gl_nn import GraphLabNeuralNetBuilder
 
 
 class GraphLabNeuralNetTest(unittest.TestCase):
 
     def test_convolution_layer(self):
-        nn = GraphLabNeuralNet()
+        nn = GraphLabNeuralNetBuilder()
         nn.add_convolution_layer(3, 1, 10)
 
         nn.add_convolution_layer(3, 1, 10, **{'padding': 1})
 
         return
+
+    def test_max_pooling_layer(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_max_pooling_layer(3)
+        nn.add_max_pooling_layer(3, padding=0)
+        nn.add_max_pooling_layer(3, 4, 5)
+
+    def test_avg_pooling_layer(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_avg_pooling_layer(3)
+        nn.add_avg_pooling_layer(3, padding=0)
+        nn.add_avg_pooling_layer(3, 4, 5)
+
+    def test_flatten_layer(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_flatten_layer()
+
+    def test_full_connection_layer(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_full_connection_layer(10)
+        self.assertEqual(len(nn.layers), 2)
+        self.assertEqual(nn.layers[0].name, 'flatten')
+
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_avg_pooling_layer(3)
+        nn.add_full_connection_layer(10)
+        self.assertEqual(len(nn.layers), 3)
+
+    def test_activation_layers(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_relu_layer()
+        nn.add_sigmoid_layer()
+        nn.add_tanh_layer()
+
+    def test_drop_out_layer(self):
+        nn = GraphLabNeuralNetBuilder()
+        nn.add_dropout_layer()
+        nn.add_dropout_layer(.8)
