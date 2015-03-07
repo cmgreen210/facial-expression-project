2ea68d4b633aab8f4143b284cd1e7df7aa909174
Chris Green <cmgreen210@gmail.com>
2015-03-07 11:01:35 -0800

    Simplifying data import for normalization

--- fec/classifier/gl_data.py
+++ fec/classifier/gl_data.py
@@ -1,5 +1,5 @@
 import numpy as np
-import graphlab as gl
+#import graphlab as gl
 from boto.s3.connection import S3Connection
 import os
 from filechunkio import FileChunkIO
@@ -7,6 +7,7 @@ import math
 import shutil
 import skimage.io
 import fec.media.helpers.utilities as utilities
+import pandas as pd
 
 
 _conn = None
@@ -35,6 +36,51 @@ def upload_big_file(source_path, bucket):
 
     mp.complete_upload()
 
+
+def _load_original_data_into_df(file_path):
+    df = pd.read_csv(file_path)
+    df = df.rename(columns={'Usage': 'usage', 'Emotion': 'label'})
+    df['pixels'] = df['pixels'].apply(lambda x:
+                                      np.fromstring(x, sep=' ', dtype=int))
+    return df
+
+
+def read_data_in_and_save_df(file_path, raw_dir):
+
+    f = open(file_path)
+    f.readline()  # Ignore first
+
+    count = 0
+    if os.path.exists(raw_dir):
+        shutil.rmtree(raw_dir)
+
+    os.makedirs(raw_dir)
+
+    csv_out = os.path.join(raw_dir, 'images.csv')
+    f_img = open(csv_out, 'w')
+
+    targets = []
+    for line in f:
+        s = line.split(',')
+        emotion = int(s[0])
+        targets.append(emotion)
+
+        suf = ''
+        if len(s) > 2:
+            suf = str(s[2]).lower().strip()
+
+        s = s[1].replace('"', '').replace(' ', ',')
+        s += '\n'
+        f_img.write(s)
+
+    f_img.close()
+
+    target_path = os.path.join(raw_dir, 'target.txt')
+    f = open(target_path, 'w')
+
+    for t in targets:
+        print >> f, t
+    f.close()
 
 def read_data_in_and_save(file_path, raw_dir, rotate=None):
         """Read kaggle training data text file
@@ -128,27 +174,29 @@ def create_sframe(img_paths, target_path):
 
 
 if __name__ == '__main__':
-    tmp_dir = os.environ['HOME']
-    tmp_dir = os.path.join(tmp_dir, '.tmp/')
-    if not os.path.exists(tmp_dir):
-        os.mkdir(tmp_dir)
-
-    data_path = os.path.join(os.path.dirname(__file__), 'data/fer2013.csv')
-
-    rotations = [90, 180, 270]
-
-    img_paths, target_path = read_data_in_and_save(data_path, tmp_dir,
-                                                   rotate=rotations)
-    sf = create_sframe(img_paths, target_path)
-
-    sf.save('s3://cmgreen210-emotions/sframe-full')
-
+    # tmp_dir = os.environ['HOME']
+    # tmp_dir = os.path.join(tmp_dir, '.tmp/')
+    # if not os.path.exists(tmp_dir):
+    #     os.mkdir(tmp_dir)
     #
-    # rot_mat = [get_rotation_matrix(48, 48, r) for r in rotations]
+    # data_path = os.path.join(os.path.dirname(__file__), 'data/fer2013.csv')
     #
-    # for s in sf:
-    #     image = s['images']
+    # rotations = [90, 180, 270]
     #
-
-    if os.path.exists(tmp_dir):
-        shutil.rmtree(tmp_dir)
+    # img_paths, target_path = read_data_in_and_save(data_path, tmp_dir,
+    #                                                rotate=rotations)
+    # sf = create_sframe(img_paths, target_path)
+    #
+    # sf.save('s3://cmgreen210-emotions/sframe-full')
+    #
+    # #
+    # # rot_mat = [get_rotation_matrix(48, 48, r) for r in rotations]
+    # #
+    # # for s in sf:
+    # #     image = s['images']
+    # #
+    #
+    # if os.path.exists(tmp_dir):
+    #     shutil.rmtree(tmp_dir)
+    read_data_in_and_save_csv('/Users/chris/Downloads/fer2013/fer2013.csv',
+                              '/Users/chris/tmp/raw/')
