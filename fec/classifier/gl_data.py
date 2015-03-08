4df63f6dce2318b6846009d31fa35c75b8e3c9ea
Chris Green <cmgreen210@gmail.com>
2015-03-08 15:00:39 -0700

    gl_data update

--- fec/classifier/gl_data.py
+++ fec/classifier/gl_data.py
@@ -1,12 +1,8 @@
 import numpy as np
-import graphlab as gl
 from boto.s3.connection import S3Connection
 import os
 from filechunkio import FileChunkIO
 import math
-import shutil
-import skimage.io
-import fec.media.helpers.utilities as utilities
 import pandas as pd
 
 
@@ -45,115 +41,12 @@ def _load_original_data_into_df(file_path):
     return df
 
 
-# def read_data_in_and_save(file_path, raw_dir, rotate=None):
-#         """Read kaggle training data text file
-#
-#         """
-#         image_paths = []
-#         targets = []
-#
-#         f = open(file_path)
-#         f.readline()  # Ignore first
-#
-#         count = 0
-#         if os.path.exists(raw_dir):
-#             shutil.rmtree(raw_dir)
-#
-#         os.makedirs(raw_dir)
-#
-#         if rotate is not None:
-#             rot_mat = [utilities.get_rotation_matrix(48, 48, r)
-#                        for r in rotate]
-#
-#         for line in f:
-#             s = line.split(',')
-#             emotion = int(s[0])
-#
-#             suf = ''
-#             if len(s) > 2:
-#                 suf = str(s[2]).lower().strip()
-#
-#             s = s[1].replace('"', '').split()
-#
-#             data_ = np.array(s, dtype=int).reshape(48, 48)
-#             data_ = data_.astype('uint8')
-#
-#             image_path = os.path.join(raw_dir,
-#                                       'im_' + str(count) +
-#                                       '_' + suf + '.png')
-#
-#             skimage.io.imsave(image_path,
-#                               data_)
-#             image_paths.append(image_path)
-#             targets.append(emotion)
-#
-#             if rotate is not None:
-#                 cnt = 0
-#                 flip_image = utilities.flip_image(data_)
-#                 for m in rot_mat:
-#                     img_rot = utilities.rotate_image(data_, m)
-#                     image_path = os.path.join(raw_dir,
-#                                               'im_' + str(count) +
-#                                               '_' + suf + '_' + str(cnt) +
-#                                               '.png')
-#
-#                     skimage.io.imsave(image_path, img_rot)
-#
-#                     image_paths.append(image_path)
-#                     targets.append(emotion)
-#                     cnt += 1
-#
-#                     img_rot = utilities.rotate_image(flip_image, m)
-#                     image_path = os.path.join(raw_dir, 'im_' + str(count) +
-#                                               '_' + suf + '_' + str(cnt) +
-#                                               '.png')
-#
-#                     skimage.io.imsave(image_path, img_rot)
-#
-#                     image_paths.append(image_path)
-#                     targets.append(emotion)
-#                     cnt += 1
-#
-#             count += 1
-#
-#         target_path = os.path.join(raw_dir, 'target.txt')
-#         f = open(target_path, 'w')
-#
-#         for t in targets:
-#             print >> f, t
-#         f.close()
-#
-#         return image_paths, target_path
-
-
 if __name__ == '__main__':
-    # tmp_dir = os.environ['HOME']
-    # tmp_dir = os.path.join(tmp_dir, '.tmp/')
-    # if not os.path.exists(tmp_dir):
-    #     os.mkdir(tmp_dir)
-    #
-    # data_path = os.path.join(os.path.dirname(__file__), 'data/fer2013.csv')
-    #
-    # rotations = [90, 180, 270]
-    #
-    # img_paths, target_path = read_data_in_and_save(data_path, tmp_dir,
-    #                                                rotate=rotations)
-    # sf = create_sframe(img_paths, target_path)
-    #
-    # sf.save('s3://cmgreen210-emotions/sframe-full')
-    #
-    # #
-    # # rot_mat = [get_rotation_matrix(48, 48, r) for r in rotations]
-    # #
-    # # for s in sf:
-    # #     image = s['images']
-    # #
-    #
-    # if os.path.exists(tmp_dir):
-    #     shutil.rmtree(tmp_dir)
-    # # read_data_in_and_save_csv('/Users/chris/Downloads/fer2013/fer2013.csv',
-    # #                           '/Users/chris/tmp/raw/')
+
     fer_data = '/Users/chris/Downloads/fer2013/fer2013.csv'
     df = _load_original_data_into_df(fer_data)
-    sf = gl.SFrame(df)
-    sf.save('s3://cmgreen210-emotions/sframe_3col_35k')
+    df.to_pickle('/Users/chris/tmp/fer_data.pkl')
+    con = get_connection()
+    bucket = con.get_bucket('cmgreen210-emotions')
+    upload_big_file('/Users/chris/tmp/fer_data.pkl',
+                    bucket)
