7e4441afb9c43358bfa66c31223e6ca90d4cef7a
EC2 Default User <ec2-user@ip-172-31-4-127.us-west-1.compute.internal>
2015-03-09 20:20:22 +0000

    update start_email script

--- start_email.sh
+++ start_email.sh
@@ -5,7 +5,6 @@ BASE_DIR="/home/ec2-user/face-emotion-classifier"
 CHK_DIR="${BASE_DIR}/checkpoint/${RUN_NAME}"
 
 crontab -l > mycron
-echo "*/5 * * * * /usr/lib/sendmail cmgreen210@gmail.com" < \
-        "${CHK_DIR}/output.txt"
+echo "*/7 * * * * /usr/lib/sendmail cmgreen210@gmail.com < ${CHK_DIR}/output.txt" >> mycron
 crontab mycron
 rm mycron
