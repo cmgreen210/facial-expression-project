5c46d9902558d8bd4dd841ec08add405876c96a6
Chris Green <cmgreen210@gmail.com>
2015-03-09 16:44:33 -0700

    create output script

---
+++ create_output_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME=$1
+
+mkdir "checkpoint/${NAME}"
+
+echo "learning_rate, 0.001, f
+batch_size, 128,i
+learning_rate_schedule, constant  ,
+subtract_mean, False,b
+random_mirror, True, b" > "checkpoint/${NAME}/output.txt"
