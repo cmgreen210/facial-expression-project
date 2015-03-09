da7b2acae27c2920639215eacf2239f439b06cfc
Chris Green <cmgreen210@gmail.com>
2015-03-09 16:15:57 -0700

    update train script

--- train_cnn.py
+++ train_cnn.py
@@ -126,6 +126,8 @@ if __name__ == '__main__':
     check_point_path = sys.argv[2]
     data_path = sys.argv[3]
     max_iterations = int(sys.argv[4])
+    if sys.argv[5] == '':
+        exit()
     model = int(sys.argv[5])
 
     if model == 1:
