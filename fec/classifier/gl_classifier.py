6d507c7badd0ef8ca870efd0bc12255ef76f9223
Chris Green <cmgreen210@gmail.com>
2015-03-09 07:19:18 -0700

    train script update

--- fec/classifier/gl_classifier.py
+++ fec/classifier/gl_classifier.py
@@ -101,13 +101,7 @@ class GraphLabClassifierFromNetBuilder(ClassifierBase):
             verbose=self._verbose,
             validation_set=valid_set,
             metric=['accuracy',
-                    'recall@1',
                     'recall@2',
-                    'recall@3',
-                    'recall@4',
-                    'recall@5',
-                    'recall@6',
-                    'recall@7'
                     ]
         )
 
