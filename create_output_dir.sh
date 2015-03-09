d7dbb2175d63fc4cf8b6bcc9829686f3f07cf7ad
Chris Green <cmgreen210@gmail.com>
2015-03-09 16:59:04 -0700

    update output script

--- create_output_dir.sh
+++ create_output_dir.sh
@@ -5,7 +5,8 @@ NAME=$1
 mkdir "checkpoint/${NAME}"
 
 echo "learning_rate, 0.001, f
-batch_size, 128,i
+batch_size, 256,i
 learning_rate_schedule, constant  ,
 subtract_mean, False,b
-random_mirror, True, b" > "checkpoint/${NAME}/net_params.txt"
+random_mirror, True, b
+momentum, 0.9, f" > "checkpoint/${NAME}/net_params.txt"
