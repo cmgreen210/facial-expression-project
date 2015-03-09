dd11e7cb33e8bfae988d6820c550bb50084a8cc5
Chris Green <cmgreen210@gmail.com>
2015-03-09 11:08:29 -0700

    bash update

--- train_nn.sh
+++ train_nn.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-RUN_NAME = $1
-MAX_IT = $2
+RUN_NAME=$1
+MAX_IT=$2
 
-BASE_DIR = "/home/ec2-user/face-emotion-classifier"
-CHK_DIR = "${BASE_DIR}/checkpoint/${RUN_NAME}"
-PARAMS = "${CHK_DIR}/net_params.txt"
-DATA = "/home/ec2-user/data/fer_data.pkl"
+BASE_DIR="/home/ec2-user/face-emotion-classifier"
+CHK_DIR="${BASE_DIR}/checkpoint/${RUN_NAME}"
+PARAMS="${CHK_DIR}/net_params.txt"
+DATA="/home/ec2-user/data/fer_data.pkl"
 
-OUTPUT = "${CHK_DIR}/output.txt"
+OUTPUT="${CHK_DIR}/output.txt"
 
 /usr/local/bin/python train_cnn.py "${PARAMS}" "${CHK_DIR}" "${DATA}" > "${OUTPUT}"
 
