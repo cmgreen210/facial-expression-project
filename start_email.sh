cd55c8ae88bed27eba60cab7e6ff6c8a1fc612be
Chris Green <cmgreen210@gmail.com>
2015-03-09 12:59:58 -0700

    email updates

---
+++ start_email.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RUN_NAME=$1
+BASE_DIR="/home/ec2-user/face-emotion-classifier"
+CHK_DIR="${BASE_DIR}/checkpoint/${RUN_NAME}"
+
+crontab -l > mycron
+echo "*/5 * * * * /usr/lib/sendmail cmgreen210@gmail.com" < \
+        "${CHK_DIR}/output.txt"
+contab mycron
+rm mycron
