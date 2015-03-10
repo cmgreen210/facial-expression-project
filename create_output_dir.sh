4e77f39e953c6313d0ffde2781551ce87c758981
Chris Green <cmgreen210@gmail.com>
2015-03-10 11:52:30 -0700

    add parameters to initial writing of net parameters

--- create_output_dir.sh
+++ create_output_dir.sh
@@ -5,8 +5,17 @@ NAME=$1
 mkdir "checkpoint/${NAME}"
 
 echo "learning_rate, 0.001, f
-batch_size, 256,i
+batch_size, 100,i
 learning_rate_schedule, constant  ,
-subtract_mean, False,b
+subtract_mean, True,b
 random_mirror, True, b
-momentum, 0.9, f" > "checkpoint/${NAME}/net_params.txt"
+momentum, 0.9, f
+l2_regularization, 0.0005, f
+min_learning_rate, 0.00001, f
+learning_rate_alpha, 0.5, f
+learning_rate_gamma, 01, f
+learning_rate_step, 1, i
+init_random, gaussian,
+init_sigma, 0.01,f
+init_bias, 0.0,f
+divideby, 255, f" > "checkpoint/${NAME}/net_params.txt"
