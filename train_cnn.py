3e812b923a02436fa97e12da52555b3853bb119d
Chris Green <cmgreen210@gmail.com>
2015-03-10 11:31:57 -0700

    update training script for only three labels

--- train_cnn.py
+++ train_cnn.py
@@ -1,16 +1,18 @@
-import graphlab as gl
+from __future__ import print_function
 from fec.classifier.gl_nn import GraphLabNeuralNetBuilder
 from fec.classifier.gl_classifier import GraphLabClassifierFromNetBuilder
 import pandas as pd
 import numpy as np
 import sys
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import precision_score, f1_score
+import os.path.join as pjoin
 
 
-def create_gl_default(net, conv_channels=10, hidden_channels=100):
+def simple_net_1(net):
     #   ----Conv
     stride = 1
-    num_channels = conv_channels
+    num_channels = 32
     kernel_size = 5
 
     kwargs = {'padding': 2}
@@ -27,7 +29,7 @@ def create_gl_default(net, conv_channels=10, hidden_channels=100):
     net.add_flatten_layer()
 
     #-----Fully Connected-----
-    num_hidden_units = hidden_channels
+    num_hidden_units = 256
 
     net.add_full_connection_layer(num_hidden_units)
 
@@ -35,11 +37,11 @@ def create_gl_default(net, conv_channels=10, hidden_channels=100):
     net.add_relu_layer()
 
     #----Drop Out----
-    threshold = 0.5
-    net.add_dropout_layer(threshold=0.5)
+    threshold = 0.3
+    net.add_dropout_layer(threshold=threshold)
 
     #-----Fully Connected-----
-    num_hidden_units = 7
+    num_hidden_units = 3
 
     net.add_full_connection_layer(num_hidden_units)
 
@@ -185,26 +187,51 @@ if __name__ == '__main__':
     model = int(sys.argv[5])
 
     if model == 1:
-        net = create_net_kag(net)
-    elif model == 2:
-        net = create_gl_default(net)
-    elif model == 3:
-        net = create_gl_default(net, 32)
-    elif model == 4:
-        net = create_net_2_conv(net)
+        net = simple_net_1(net)
     else:
         exit()
 
     df = pd.read_pickle(data_path)
+
+    cond_happy = df['emotion'] == 3
+    cond_sad = df['emotion'] == 2
+    cond_surprise = df['emotion'] == 5
+
+    df = df[cond_happy | cond_sad | cond_surprise]
+
     x = np.array(df['pixels'].tolist())
     y = np.array(df['emotion'].values)
 
-    xtrain, xtest, ytrain, ytest = train_test_split(x, y, train_size=.9)
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, train_size=.8)
 
     model = GraphLabClassifierFromNetBuilder(net, chkpt_dir=check_point_path,
                                              max_iterations=max_iterations,
-                                             train_frac=.91)
+                                             train_frac=.8)
     model.fit(xtrain, ytrain)
 
-    print model.evaluate(xtest, ytest, metric=['accuracy', 'confusion_matrix',
+    eval = model.evaluate(xtest, ytest, metric=['accuracy', 'confusion_matrix',
                                                'recall@1', 'recall@2'])
+
+    ypred = np.array(model.predict(xtest))
+    ytest = np.array(ytest)
+
+    test_f1 = f1_score(ytest, ypred)
+    test_precision = f1_score(ytest, ypred)
+
+    result_file = open(pjoin(check_point_path, 'results.txt'))
+
+    write = lambda val: print(val, file=result_file)
+    write('accuracy, {0:1.6f}'.format(eval['accuracy']))
+    write('recall@1, {0:1.6f}'.format(eval['recall@1']))
+    write('recall@2, {0:1.6f}'.format(eval['recall@2']))
+    write('precision, {0:1.6f}'.format(test_precision))
+    write('f1, {0:1.6f}'.format(test_f1))
+    write('')
+    write('target_label, predicted_label, count')
+    for row in eval['confusion_matrix']:
+        write('{0}, {1}, {2}'.format(
+            row['target_label'],
+            row['predicted_label'],
+            row['count']
+        ))
+    result_file.close()
